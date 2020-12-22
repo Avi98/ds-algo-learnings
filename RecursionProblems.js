207d1b44768dbc61a5939b0cd2a9e5a06fe9bfb4
Avinash <9958703925.dad@gmail.com>
2020-12-22 20:48:14 +0530

    Update RecursionProblems.js

--- RecursionProblems.js
+++ RecursionProblems.js
@@ -3,7 +3,7 @@
 // implement deepclone functionlity
 // implement power
 // implement fib
-
+// someRecursive Solution
 
 
 //lodash get problem
@@ -17,5 +17,46 @@ function getProp( object, keys, defaultVal ){
   return object === undefined? defaultVal : object;
 }
 
+// fib 0,1,1,2,3,5,8
 
-// 
+function fibRecursive(num){
+  if(num < 2){
+    return num
+  }
+  return fib(num-1) + fib(num-2);
+}
+
+//for 1 fib will always be 1, cause 0+1
+function fibItrative(num){
+  let sumArr = [0,1]
+  
+  for(let i=0;i<=num; i++){
+    if(i > 1){
+     const a = sumArr[i-1]
+     const b = sumArr[i-2]
+     
+     sumArr.push(a+b)
+    }
+  }
+  
+  return sumArr
+}
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+function someRecursive(arr, cb){
+  if(arr.length === 0){
+    return
+  }
+  const val = arr.shift()
+  const res = cb(val)
+  someRecursive(arr, cb)
+  
+  return res
+}
+
+
+
